028e34afdcb7bc379b18d5b6140d524dbeb12c5c
Kanu Chukwunonso <franknonso114@gmail.com>
2022-04-28 10:51:39 +0100

    feat: update react-scripts to v5

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,7 @@ import UserSearch from '../components/users/UserSearch'
 function Home() {
   return (
     <>
+      <h2 class="mb-4 font-bold">Search for any Github user or repositories.</h2>
       <UserSearch />
       <UserResults />
     </>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  // content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
     extend: {},
   },
